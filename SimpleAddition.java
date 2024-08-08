d3260b85d6b808c54a1ddc0ea42ed0aefd41e0ff
Vanshita <123195617+Vanshup@users.noreply.github.com>
2024-08-08 11:09:45 +0530

    Add files via upload

---
+++ SimpleAddition.java
@@ -0,0 +1,24 @@
+public class SimpleAddition {
+    public static void main(String[] args) {
+        // Define some numbers
+        int num1 = 10;
+        int num2 = 5;
+
+        // Perform arithmetic operations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        // Display the results
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
+
